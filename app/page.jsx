e8ab27036203e0009fe0dcb955897fb18475a5dd
Callum Laing <callum.laing91@hotmail.co.uk>
2024-03-01 01:23:02 +0000

    shop page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ import styles from "./page.module.css";
 
 function Home() {
   return (
-    <main className={styles.container}>{/* <h1>Hello world!</h1> */}</main>
+    <main className={styles.container}>
+      <img className={styles.shopImg} src="/shop.jpg" alt="shop front image" />
+    </main>
   );
 }
 
--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -3,7 +3,12 @@ import Styles from "./page.module.css";
 export default function Shop() {
   return (
     <main>
-      <h2 className={Styles.title}>Shop</h2>
+      <h1>Shop Page</h1>
+      <p>
+        This needs to be a grid layout that has 3-4 products across, and then
+        goes down as many as it needs to, to show all of the products(API
+        perhaps?)
+      </p>
     </main>
   );
 }
