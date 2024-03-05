aec7bd0df736f9f03d7074f93e4e335e1a6d7189
Callum Laing <callum.laing91@hotmail.co.uk>
2024-03-05 23:40:33 +0000

    fixed mapping in shop

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import Card from "../components/Card";
 import Styles from "./page.module.css";
 import posts from "../data";
 
 const Shop = () => {
   return (
     <main className={Styles.shopContainer}>
-      <div className={Styles.cards}>
-        {posts.map((post) => (
-          <Card key={post.id} {...post} />
-        ))}
-      </div>
+      {posts.map((post) => (
+        <div key={post.id} className={Styles.card}>
+          <img className="imgCard" src={`/cleats.jpg`} alt={post.name} />
+          <h2>{post.name}</h2>
+          <p>${post.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      ))}
     </main>
   );
 };
