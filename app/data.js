aaa0a273e76d63c5a544f8186f15427aa3fcb8b4
Callum Laing <callum.laing91@hotmail.co.uk>
2024-03-06 23:37:39 +0000

    added new images to shop page

--- app/data.js
+++ app/data.js
@@ -1,11 +1,30 @@
 const posts = [
-  { id: 1, name: "Jordan One", price: 39.99, image: "/public/cleats.jpg" },
-  { id: 2, name: "Jordan One", price: 12.99, image: "/public/cleats.jpg" },
-  { id: 3, name: "Jordan One", price: 16.99, image: "/public/cleats.jpg" },
-  { id: 4, name: "Jordan One", price: 13.99, image: "/public/cleats.jpg" },
-  { id: 5, name: "Jordan One", price: 14.99, image: "/public/cleats.jpg" },
-  { id: 6, name: "Jordan One", price: 29.99, image: "/public/cleats.jpg" },
-  // Add more images as needed
+  {
+    id: 1,
+    name: "Shoe One",
+    price: 39.99,
+    image: "/shoes/one.jpg",
+  },
+  {
+    id: 2,
+    name: "Shoe Two",
+    price: 79.99,
+    image: "/shoes/two.jpg",
+  },
+  {
+    id: 3,
+    name: "Shoe Three",
+    price: 49.99,
+    image: "/shoes/three.jpg",
+  },
+  { id: 4, name: "Shoe Four", price: 39.99, image: "/shoes/four.jpg" },
+  { id: 5, name: "Shoe Five", price: 49.99, image: "/shoes/five.jpg" },
+  { id: 6, name: "Shoe Six", price: 89.99, image: "/shoes/six.jpg" },
+  { id: 6, name: "Shoe Seven", price: 79.99, image: "/shoes/seven.jpg" },
+  { id: 6, name: "Shoe Eight", price: 49.99, image: "/shoes/eight.jpg" },
+  { id: 6, name: "Shoe Nine", price: 89.99, image: "/shoes/nine.jpg" },
+  { id: 6, name: "Shoe Ten", price: 59.99, image: "/shoes/ten.jpg" },
+  { id: 6, name: "Shoe Eleven", price: 69.99, image: "/shoes/eleven.jpg" },
 ];
 
 export default posts;
--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Styles from "./page.module.css";
 import posts from "../data";
+import Image from "next/image";
 
 const Shop = () => {
   return (
     <main className={Styles.shopContainer}>
       {posts.map((post) => (
         <div key={post.id} className={Styles.card}>
-          <img className="imgCard" src={`/cleats.jpg`} alt={post.name} />
+          <Image
+            className="imgCard"
+            src={post.image}
+            alt={post.name}
+            width={450}
+            height={600}
+          />
           <h2>{post.name}</h2>
           <p>Price: ${post.price}</p>
           <button className={Styles.btn}>Add to Cart</button>
